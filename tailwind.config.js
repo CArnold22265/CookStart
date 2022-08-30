c0307b493993b540a8466638222f778d7c683e04
Christopher Arnold <78048184+CArnold22265@users.noreply.github.com>
2022-08-29 22:53:58 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontfamily: {
+      'roboto': ['roboto, sans-serif'],
+      'roboto-slab': ['roboto, serif'],
+      
+    },
+    screens: {
+      sm: '480px',
+      md: '720px',
+      lg: '1076px',
+      xl: '1440px'
+    },
+    extend: {
+      fontFamily: {
+        marker: ["Permanent Marker", "cursive"],
+      },
+      colors: {
+        blu100: '#D3DFFF',
+        blu200: '#ACC2FF',
+        blu300: '#5E89ff',
+        blu400: '#366CFF',
+        blu500: '#0f50ff',
+        blumain: '#003ee7',
+        PinkishAccent: '#e7003e',
+        GreenAccent: '#3ee700',
+      },
+    },
+  },
+  plugins: [],
+}
+
