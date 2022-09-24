0f7a8ff2204f4ac9632033b95dd1117f93030b35
Christopher Arnold <78048184+CArnold22265@users.noreply.github.com>
2022-09-24 15:53:57 -0700

    i h8 css

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+document.querySelector(".button").addEventListener("mouseover", function () {
+  //   const btn = document.querySelector(".button");
+  document
+    .querySelector(".button")
+    .setAttribute("style", "backgound-color:Red;");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     fontfamily: {
-      'roboto': ['roboto, sans-serif'],
-      'roboto-slab': ['roboto, serif'],
-      
+      roboto: ["roboto, sans-serif"],
+      "roboto-slab": ["roboto, serif"],
     },
     screens: {
-      sm: '480px',
-      md: '720px',
-      lg: '1076px',
-      xl: '1340px'
+      sm: "480px",
+      md: "720px",
+      lg: "1076px",
+      xl: "1340px",
     },
     extend: {
       fontFamily: {
         marker: ["Permanent Marker", "cursive"],
       },
       colors: {
-        blu100: '#D3DFFF',
-        blu200: '#ACC2FF',
-        blu300: '#5E89ff',
-        blu400: '#366CFF',
-        blu500: '#0f50ff',
-        blumain: '#003ee7',
-        PinkishAccent: '#e7003e',
-        GreenAccent: '#3ee700',
-        MainCompliment: '#E73500',
+        blu100: "#D3DFFF",
+        blu200: "#ACC2FF",
+        blu300: "#5E89ff",
+        blu400: "#366CFF",
+        blu500: "#0f50ff",
+        blumain: "#003ee7",
+        PinkishAccent: "#e7003e",
+        GreenAccent: "#3ee700",
+        MainCompliment: "#E73500",
       },
     },
   },
   plugins: [],
-}
-
+};
