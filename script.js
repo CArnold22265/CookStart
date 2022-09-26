758a6841a752740eeb3e99570ff1867d86f7fa87
Christopher Arnold <78048184+CArnold22265@users.noreply.github.com>
2022-09-25 18:45:42 -0700

    hero so close

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 "use strict";
-document.querySelector(".button").addEventListener("mouseover", function () {
-  //   const btn = document.querySelector(".button");
-  document
-    .querySelector(".button")
-    .setAttribute("style", "backgound-color:Red;");
-});
