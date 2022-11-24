3212e542dcd43c7bd6fb09d220795ab10333220f
Christopher Arnold <78048184+CArnold22265@users.noreply.github.com>
2022-11-23 22:16:58 -0800

    poll html and button

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 "use strict";
 console.log("script is connected");
+
+const pollSubmitBtn = document.querySelector(".pollBtn");
+
+const btnClicked = function (e) {
+  e.preventDefault();
+  console.log("CLICKED");
+};
+console.log(pollSubmitBtn.value);
+pollSubmitBtn.addEventListener("click", function (e) {
+  //e.preventDefault();
+  console.log("CLICKED");
+});
