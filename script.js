569ace04c0fcdc42522d93c3688887100c1b790b
Christopher Arnold <78048184+CArnold22265@users.noreply.github.com>
2022-11-27 20:22:25 -0800

    cant find which radio is checked...

--- script.js
+++ script.js
@@ -1,48 +1,78 @@
 "use strict";
 /////SECTION POLL VARIABLES
 //Radio buttons
-const pollQ1Radio = document.querySelector(".poll-q1");
-const pollQ2Radio = document.querySelector(".poll-q2");
-const pollQ3Radio = document.querySelector(".poll-q3");
-const pollQ4Radio = document.querySelector(".poll-q4");
+
+const pollQ1Radio = document.getElementById("lessons");
+const pollQ2Radio = document.getElementById("poll-recipes");
+const pollQ3Radio = document.getElementById("poll-recipe-book");
+const pollQ4Radio = document.getElementById("poll-users-sec");
+//poll submit btn
 const pollSubmitBtn = document.querySelector(".pollBtn");
+
+// radio button array
+const pollRadios = [pollQ1Radio, pollQ2Radio, pollQ3Radio, pollQ4Radio];
 
 //Labels
 const pollQ1Label = document.getElementById("poll-q1_label");
 const pollQ2Label = document.getElementById("poll-q2_label");
 const pollQ3Label = document.getElementById("poll-q3_label");
 const pollQ4Label = document.getElementById("poll-q4_label");
-console.log(pollQ1Label);
-//POLL ARRAY
-const pollRadioBtns = [pollQ1Radio, pollQ2Radio, pollQ3Radio, pollQ4Radio];
+// LABEL ARRAY
+const pollLabels = [pollQ1Label, pollQ2Label, pollQ3Label, pollQ4Label];
+
+//unhide top of poll
+const unhideVal1 = document.getElementById("unhideQ1");
+const unhideVal2 = document.getElementById("unhideQ2");
+const unhideVal3 = document.getElementById("unhideQ3");
+const unhideVal4 = document.getElementById("unhideQ4");
+
+//UNHIDE ARRAY
+const unhideVals = [unhideVal1, unhideVal2, unhideVal3, unhideVal4];
 
 const btnClicked = function (e) {
   e.preventDefault();
   console.log("CLICKED");
 };
 
-/*
-BUG
-1.) ADD CLASS OF HIDDEN TO DIVS ABOVE LABELS
-2.) REMOVE HIDDEN CLASS IN EVENT LISTENER BELOW
-3.) CREATE ARRAY LOOPS EXTERNALLY FROM FUNCTION BELOW TO KEEP CODE DRY
-4.)CALL LOOPS IN FUNCTION BELOW
-5.) Add class of  no display to radio buttons after click.
-6.)hide yellow backgrounds...
-7.) function which reads poll scores as array values and fills the backgrounds accordingly 
-8.) create functions with paramaters of class to be removed and variable storing the element to remove the class from.
+//ADD A CLASS FUNCTION
+const addClass = function (arr, addClass) {
+  const newV = [];
 
-*/
-console.log(pollSubmitBtn.value);
+  arr.map((el) => {
+    newV.push((el.className += addClass));
+  });
+};
+
+/* 1.)create variable pollAnswers which contains values selected in the poll
+   2.)create a variable percentageVotes that shallow copies a map of pollAnswers to determine what percentage of the poll each value equates to
+   3.) modify poll q bg class as a transition which eases-in, and fills to a width === percentageVotes
+   4..) loop answers array, if radio is checked at position splice poll answers at i, or try pollAnswers[i]++ 
+  */
+
+//SECTION
+const pollAnswers = [22, 25, 21, 27];
+
+const pollAnswersSum = pollAnswers.reduce((acc, cur) => acc + cur, 0);
+
+const percentageVotes = pollAnswers.map((a, i, array) => {
+  return Math.trunc((a / pollAnswersSum) * 100);
+});
+console.log(percentageVotes);
+//BUG
+
+//POLL SUBMIT LISTENER
 pollSubmitBtn.addEventListener("click", function (e) {
-  //Remove display of text
-  pollQ1Label.classList.add("no-display");
-  pollQ2Label.classList.add("no-display");
-  pollQ3Label.classList.add("no-display");
-  pollQ4Label.classList.add("no-display");
   e.preventDefault();
-  console.log("CLICKED");
-  console.log(pollQ1Label.classList);
+  //Remove display of text
+  addClass(pollLabels, " no-display");
+  //hide radio btns
+  addClass(pollRadios, " no-display");
+  //Add text above radios
+  addClass(unhideVals, " yes-display");
+
+  //Update answers array..
+  const radioButtons = document.querySelectorAll('input[name="radioPoll"]');
+  pollRadios.filter;
 });
 
 const clickedRadio = function (rad) {
