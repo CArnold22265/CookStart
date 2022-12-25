38219c8382c384a15b62449f689669e2780156d2
Christopher Arnold <78048184+CArnold22265@users.noreply.github.com>
2022-12-24 21:16:33 -0800

    grr

--- lessons/script.js
+++ lessons/script.js
@@ -3,21 +3,25 @@
 const lesson1 = document.querySelector(".lesson-1");
 const lesson2 = document.querySelector(".lesson-2");
 const lesson3 = document.querySelector(".lesson-3");
-const lessons = [lesson1, lesson2, lesson3];
+const lessons = document.querySelectorAll(".lesson");
 const arrowBtnLeft = document.getElementById("arrow-left");
 const arrowBtnRight = document.getElementById("arrow-right");
-
+const eventParent = document.querySelector(".les-parent");
 arrowBtnRight.addEventListener("click", function (e) {
-  lessons.forEach((el, i, arr) => {
-    if (!arr.at(i).classList.contains("hidden")) {
-      arr.at(i).classList.add("hidden");
-      arr.at(i++).classList.remove("hidden");
-      if (i != arr.length - 1) {
-        arr.at(i + 1).classList.remove("hidden");
-      }
-      return;
-    }
+  const next = Array.from(lessons, (el, i, arr) => {
+    el.classList.add("hidden");
   });
+  //Array.from([1, 2, 3], (x) => x + x);
+  console.log(next);
+  // next.forEach((element) => {});
+  // (el, i, arr) => {
+  //   if (!arr.at(i).classList.contains("hidden")) {
+  //     arr.at(i).classList.add("hidden");
+  //     arr.at(i++).classList.remove("hidden");
+  //     if (i != arr.length - 1) {
+  //       next[i + 1].classList.remove("hidden");
+  //     }
+  //     return;
 });
 
 // function handleClickRight(event) {
